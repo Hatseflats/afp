8a6358ea38946dccb9931d8589ea9a71472f3057
Sebastiaan Jong <sebastiaan.jong@gmail.com>
2014-09-20 13:44:08 +0200

    cleanup

---
+++ 25.hs
@@ -0,0 +1,18 @@
+--2.5
+
+test :: [Int]
+test = [count, count 1 2 3, count "" [True, False] id (+)]
+
+class Counter r where
+	count' :: Int -> r
+
+instance Counter Int where
+	count' = id
+    --count' = const 0
+
+instance (Counter r) => Counter (a -> r) where
+    count' acc = \x -> count' (acc+1)
+
+count :: (Counter a) => a
+count = count' 0
+--
