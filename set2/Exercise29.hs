bbeb5e41d230f017792d324a03131d4de5f55e38
Koen Wermer <koenwermer@gmail.com>
2014-09-22 21:34:51 +0200

    2.9 afgemaakt, foutieve troep levert nu compile error ipv runtime

--- set2/Exercise29.hs
+++ set2/Exercise29.hs
@@ -1,18 +1,34 @@
-p1, p2, p3 :: Int
+{-#LANGUAGE GADTs#-}
+p1, p2 :: Int
 p1 = start store 3 store 5 add stop
 p2 = start store 3 store 6 store 2 mul add stop
-p3 = start store 2 add stop
+--p3 has no valid type
+--p3 = start store 2 add stop
 
-start = \c -> c []
+data O
+data Succ n
 
-store n = \xs c -> c (xs:n)
+-- | Data type for a list with the length as phantom type
+data ListWithLength l a where
+    Empty ::                            ListWithLength O a
+    (:+:) :: a -> ListWithLength l a -> ListWithLength (Succ l) a
 
-stop :: [Int] -> Int
-stop (x:_) = x
+infixr 5 :+:
+    
+start :: (ListWithLength O a -> b) -> b
+start c = c Empty
 
-add = \(x:y:xs) c -> c ([x+y] ++ xs)
+store :: (ListWithLength l a) -> a -> ((ListWithLength (Succ l) a) -> b) -> b
+store xs x c = c (x:+:xs)
 
-mul = \(x:y:xs) c -> c ([x*y] ++ xs)
+stop :: (ListWithLength (Succ l) a) -> a
+stop (x:+:_) = x
+
+add :: Num a => (ListWithLength (Succ (Succ l)) a) -> ((ListWithLength (Succ l) a) -> b) -> b
+add (x:+:y:+:xs) c = c ((x+y) :+: xs)
+
+mul :: Num a => (ListWithLength (Succ (Succ l)) a) -> ((ListWithLength (Succ l) a) -> b) -> b
+mul (x:+:y:+:xs) c = c ((x*y) :+: xs)
 
 main = do
 	print (p1)
