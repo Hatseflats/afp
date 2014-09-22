c6877defc7eeb5267e9cccdc32736e3ac0ab958d
Sebastiaan Jong <sebastiaan.jong@gmail.com>
2014-09-22 18:39:26 +0200

    added mul, how do i write type signatures for this shit?

--- set2/Exercise29.hs
+++ set2/Exercise29.hs
@@ -1,18 +1,20 @@
 --p1, p2, p3 :: Int
 p1 = start store 3 store 5 add stop
---p2 = start store 3 store 6 store 2 mul add stop
+p2 = start store 3 store 6 store 2 mul add stop
 --p3 = start store 2 add stop
 
 start = \c -> c []
 
 store n = \l c -> c (l:n)
 
-stop n = n
+stop (x:xs) = x
 
 add = \(s:ss:sss) c -> c ([s+ss] ++ sss)
 
+mul = \(s:ss:sss) c -> c ([s*ss] ++ sss)
+
 main = do
-	print p1
+	print p2
 
 
 
