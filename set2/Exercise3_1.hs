1ec50aecf56a2b8ceb61e450c5a7bfa25d1776f0
Koen Wermer <koenwermer@gmail.com>
2014-09-29 12:59:08 +0200

    opdracht 3 met state en counter af zonder transformers

--- set2/Exercise3_1.hs
+++ set2/Exercise3_1.hs
@@ -1,30 +1,38 @@
-import Control.Monad
+{-#LANGUAGE FlexibleInstances, MultiParamTypeClasses#-}
+import Control.Monad.State
 import Data.List
 
-data Counter = Counter {unCounter :: [(String, Int)]}
+data Counter = Counter [(String, Int)]
 
 instance Show Counter where
     show (Counter xs) = "[" ++ intercalate ", " (map (\(s, n) -> "(" ++ s ++ "=" ++ show n ++ ")") xs) ++ "]"
 
-add :: String -> Counter -> Counter
-add s (Counter []) = Counter [(s, 1)]
-add s (Counter ((s', n):xs)) | s == s'   = Counter ((s, n+1):xs)
-                             | otherwise = Counter ((s', n):(unCounter (add s (Counter xs))))
+increment :: String -> Counter -> Counter
+increment s (Counter c) = Counter $ increment' s c where
+    increment' s [] = [(s, 1)]
+    increment' s ((s', n):xs) | s == s'   = (s, n+1):xs
+                      | otherwise = (s', n):increment' s xs
                              
 emptyCounter :: Counter
 emptyCounter = Counter []
 
-data StateMonadPlus s a = StateMonadPlus {runState :: (s, Counter) -> (a, s, Counter)}
+data StateMonadPlus s a = StateMonadPlus {runStatePlus :: s -> Counter -> (a, s, Counter)}
 
 instance Monad (StateMonadPlus s) where
-    smp >>= f = StateMonadPlus $ \(s, c) -> let (x, s', c') = runState smp (s, c) in runState (f x) (s', add "bind" c')
-    return x  = StateMonadPlus $ \(s, c) -> (x, s, add "return" c)
+    smp >>= f = StateMonadPlus $ \s c -> let (x, s', c') = runStatePlus smp s c in runStatePlus (f x) s' (increment "bind" c')
+    return x  = StateMonadPlus $ \s c -> (x, s, increment "return" c)
     
 diagnostics :: StateMonadPlus s String
-diagnostics = StateMonadPlus $ \(s, c) -> let c' = add "diagnostics" c in (show c', s, c')
+diagnostics = StateMonadPlus $ \s c -> let c' = increment "diagnostics" c in (show c', s, c')
 
-test = (\(a, _, _) -> a) $ runState
+instance MonadState s (StateMonadPlus s) where
+    get = StateMonadPlus  (\s c -> (s, s, increment "get" c))
+    put s = StateMonadPlus (\_ c -> ((), s, increment "put" c))
+
+test = (\(a, _, _) -> a) $ runStatePlus
  (do return 3 >> return 4
      return 5
+     get
+     put 23089893248983489349
      diagnostics)
-    ((), emptyCounter)
+    undefined emptyCounter
