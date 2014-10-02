764ae6f00ba20fa118cb0b2cf2080c52a3215023
Koen Wermer <koenwermer@gmail.com>
2014-10-02 22:44:46 +0200

    4.1 af

---
+++ set2/Exercise41.hs
@@ -0,0 +1,10 @@
+data F a = F {unF :: F a -> a}
+
+y :: (a -> a) -> a
+y = \f -> (\x -> f (unF x x)) (F (\x -> f (unF x x)))
+
+--let's use it to define the factorial function
+fac :: Int -> Int
+fac = y f where
+    f g 0 = 1
+    f g n = n * g (n - 1)
