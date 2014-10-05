dc3c85ce3c96d32793ad8e6ae06c1e38bfa4db62
Koen Wermer <koenwermer@gmail.com>
2014-10-05 15:06:49 +0200

    4.2 en readme

---
+++ set2/Exercise42.hs
@@ -0,0 +1,51 @@
+{-#LANGUAGE FlexibleContexts, FlexibleInstances#-}
+type Square a = Square' Nil a
+data Square' t a = Zero (t (t a)) | Succ (Square' (Cons t) a)
+data Nil a = Nil
+data Cons t a = Cons a (t a)
+
+--The 2x2 identity matrix
+id2 :: Square Int
+id2 = Succ (Succ (Zero (Cons (Cons 1 (Cons 0 Nil)) (Cons (Cons 0 (Cons 1 Nil)) Nil))))
+
+--The 3x3 matrix containing the numbers 1 to 9
+oneToNine :: Square Int
+oneToNine = Succ (Succ (Succ (Zero (Cons (Cons 1 (Cons 2 (Cons 3 Nil))) (Cons (Cons 4 (Cons 5 (Cons 6 Nil))) (Cons (Cons 7 (Cons 8 (Cons 9 Nil))) Nil))))))
+
+
+--Define show for kind *->* thingies. This is necessary because we can not quantify over a in the context Show (t a)
+class ShowContainer t where
+    showc :: Show' a => t a -> String
+    
+--Unfortunately, we need to use a special show class to avoid ambiguity with (for example) show instances of lists
+class Show' a where
+    show' :: a -> String
+    
+instance Show' Int where
+    show' = show
+    
+instance Show' Char where
+    show' = show
+    
+instance ShowContainer [] where
+    showc = show . map show'
+    
+instance (ShowContainer t, Show' a) => Show' (t a) where
+    show' = showc
+
+instance (Show' a, Show' (t a)) => Show' (Cons t a) where
+    show' (Cons x r) = ' ' : show' x ++ show' r
+    
+instance ShowContainer Nil where
+    showc Nil = "\n"
+    
+instance ShowContainer t => ShowContainer (Cons t) where
+    showc (Cons x r) = ' ' : show' x ++ showc r
+    
+instance ShowContainer t => ShowContainer (Square' t) where
+    showc (Zero x) = showc x
+    showc (Succ x) = showc x
+    
+--Finally, we can make Square instance of show
+instance Show' a => Show (Square' Nil a) where
+    show = show'
---
+++ set2/README.txt
@@ -0,0 +1,16 @@
+Sebastiaan Jong - sebastiaan.jong@gmail.com - 5546303
+Koen Wermer     - koenwermer@gmail.com      - 3705951
+
+We implemented all excercises in seperate files.
+
+Additional comments:
+2.9
+We defined ListWithLength to keep track of the size of the stack on type level, to make the type checker reject invalid expressions like the example p3.
+
+4.1
+As a test, we used y to define factorial.
+
+4.2
+As a bonus we made squares instance of Show. 
+Unfortunately, our solution requires the elements of the square to be made instance of Show' (and things like lists to be made instance of ShowContainer, which is even more of a pain).
+Please let us know if you have a better solution!
