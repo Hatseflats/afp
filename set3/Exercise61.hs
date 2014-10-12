aebfe553c00a1dab560d7bffb4146e6c3266d4b0
Sebastiaan Jong <sebastiaan.jong@gmail.com>
2014-10-12 19:27:58 +0200

    6.1 explainations

--- set3/Exercise61.hs
+++ set3/Exercise61.hs
@@ -41,6 +41,41 @@ x
                                                                 Q.E.D.
 -}
 
+preservesPos = preserves (>0)
+preservesPos' = pos --> pos
+
+{-
+preservesPos will only fail if the result of (>0) differs between the pre check and the post check.
+preservesPos' will fail when the value isn't positive.
+
+	assert preservesPos (+1) (-3) == -2
+
+Will not fail despite the the result being negative.
+
+	assert preservesPos' (+1) (-3) == -2
+
+This will fail.
+-}
+
+allPos = List pos
+allPos' = Pred (all (>0))
+
+{-
+The difference between the two versions of allPos is that allPos is lazy due to the use of map.
+However, allPos' isn't lazy because of the all function. 
+This means that: 
+
+	length (assert allPos [1,2,-3])
+
+Will return 3. But the following will fail:
+
+	length (assert allPos' [1,2,-3])
+
+Since all directly evaluates the values in the list, this means that for an infinite list the following won't finish:
+	
+	assert allPos' [1..]
+-}
+
 indexCheck1 = assert index (!!) [1..5] 2
 indexCheck2 = assert index (!!) [0..7] 9
 
@@ -49,4 +84,4 @@ preserveCheck2 = assert (preserves length) (take 5) "Hello" == "Hello"
 preserveCheck3 = assert (preserves length) (take 5) "Hello world" == undefined
 
 main = do
-    print $ preserveCheck3
+    print $ all (>0) [1..5]
