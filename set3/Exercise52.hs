24879bf5e6bf121815d8fed00a660d7587f7ec76
Koen Wermer <koenwermer@gmail.com>
2014-10-15 17:45:34 +0200

    5.2

--- set3/Exercise52.hs
+++ set3/Exercise52.hs
@@ -8,27 +8,31 @@ instance (A a, B a) => A [a]
 {-
 1. B Int ||- A (Maybe (Maybe Int))
 
-class (A a) => B a                           
-------------------- (super)              
-        B a ||- A a      instance A a => A (Maybe a)                    
-        --------------------------------------------- (inst)       
-                B a ||- A (Maybe a)      instance A a => A (Maybe a)    
-                ----------------------------------------------------- (inst)
+                                                                         
+                                                                        instance A a => A (Maybe a) 
+                                                                        ---------------------------- (inst)  
+                                   instance A a => A (Maybe a)          A a ||- A (Maybe a)
+                                   ---------------------------- (inst)  -------------------- (closure)
+class (A a) => B a                 A a ||- A (Maybe a)                  A (Maybe a) ||- A (Maybe (Maybe a))
+------------------- (super)        ------------------------------------------------------------------------- (trans)
+        B a ||- A a                A a ||- A (Maybe (Maybe a))
+        ------------------------------------------------------- (trans)
                         B a ||- A (Maybe (Maybe a))
                         --------------------------- (closure)
-                                B Int ||- A (Maybe (Maybe Int))
+                        B Int ||- A (Maybe (Maybe Int))
 -}
 
 {-
 2. ∅ ||- A (Maybe [Bool])
 
-                        instance (A a, B a) => A [a]            instance A a => A (Maybe a)                          
-                        ---------------------------(inst)       -----------------------------(inst)
-                        (A a, B a) ||- A [a]                    A a ||- A (Maybe a)
-                        ------------------------(closure)       --------------------------- (closure)
-instance A Bool         (A bool, B bool) ||- A [Bool]           A [Bool] ||- A (Maybe [Bool])
----------------         ------------------------------------------------------------------- (trans)
- ∅ ||- A Bool                           (A bool, B bool) ||- A (Maybe [Bool])
-------------------------------------------------------------------------------- (trans)
-                         ∅ ||- A (Maybe [Bool])       
+
+                                                          instance (A a, B a) => A [a]            instance A a => A (Maybe a)                          
+                                                          ---------------------------(inst)       -----------------------------(inst)
+instance A Bool         instance B Bool                   (A a, B a) ||- A [a]                    A a ||- A (Maybe a)
+--------------- (inst)  ---------------- (inst)           ------------------------(closure)       --------------------------- (closure)
+ ∅ ||- A Bool            ∅ ||- B Bool                     (A Bool, B Bool) ||- A [Bool]           A [Bool] ||- A (Maybe [Bool])
+-------------------------------------- (dist)             ---------------------------------------------------------------------- (trans)
+      ∅ ||- (A Bool, B Bool)                                          (A bool, B bool) ||- A (Maybe [Bool])
+------------------------------------------------------------------------------------------------------------ (trans)
+                         ∅ ||- A (Maybe [Bool])
 -}
