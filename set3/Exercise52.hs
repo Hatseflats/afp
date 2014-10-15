91722e98714cd3072df8a40076bfd4ef40bbd2cd
Sebastiaan Jong <sebastiaan.jong@gmail.com>
2014-10-15 16:29:00 +0200

    finished 52

--- set3/Exercise52.hs
+++ set3/Exercise52.hs
@@ -6,21 +6,29 @@ instance A a => A (Maybe a)
 instance (A a, B a) => A [a]
 
 {-
-Prove:
-
 1. B Int ||- A (Maybe (Maybe Int))
-2. ∅ ||- A (Maybe [Bool])
+
+class (A a) => B a                           
+------------------- (super)              
+        B a ||- A a      instance A a => A (Maybe a)                    
+        --------------------------------------------- (inst)       
+                B a ||- A (Maybe a)      instance A a => A (Maybe a)    
+                ----------------------------------------------------- (inst)
+                        B a ||- A (Maybe (Maybe a))
+                        --------------------------- (closure)
+                                B Int ||- A (Maybe (Maybe Int))
 -}
 
 {-
-1. 
-class (A a) => B a                           
-------------------- (super)              
-		B a ||- A a      instance A a => A (Maybe a)                    
-        --------------------------------------------- (inst)       
-				B a ||- A (Maybe a)      instance A a => A (Maybe a)    
-            	----------------------------------------------------- (inst)
-              			B a ||- A (Maybe (Maybe a))
-              			--------------------------- (closure)
-              					B Int ||- A (Maybe (Maybe Int))
+2. ∅ ||- A (Maybe [Bool])
+
+                        instance (A a, B a) => A [a]            instance A a => A (Maybe a)                          
+                        ---------------------------(inst)       -----------------------------(inst)
+                        (A a, B a) ||- A [a]                    A a ||- A (Maybe a)
+                        ------------------------(closure)       --------------------------- (closure)
+instance A Bool         (A bool, B bool) ||- A [Bool]           A [Bool] ||- A (Maybe [Bool])
+---------------         ------------------------------------------------------------------- (trans)
+ ∅ ||- A Bool                           (A bool, B bool) ||- A (Maybe [Bool])
+------------------------------------------------------------------------------- (trans)
+                         ∅ ||- A (Maybe [Bool])       
 -}
