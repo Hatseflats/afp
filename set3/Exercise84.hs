b5337c0c2310614eb4695620afea4257a58cb571
Sebastiaan Jong <sebastiaan.jong@gmail.com>
2014-10-17 18:16:46 +0200

    tried to add explaination to 84

--- set3/Exercise84.hs
+++ set3/Exercise84.hs
@@ -1,23 +1,23 @@
-forceBoolList :: [Bool] -> [Bool]
-forceBoolList [] = []
-forceBoolList (x:xs)
-    | x == True = True:(forceBoolList xs)
-    | x == False = False:(forceBoolList xs)
+module Exercise84 where
 
-forceBoolList' :: [Bool] -> b -> b 
-forceBoolList' [] r = r
-forceBoolList' (x:xs) r
-    | x == True = True:(forceBoolList' xs r)
-    | x == False = False:(forceBoolList' xs r)
+forceBoolList :: [Bool] -> b -> b 
+forceBoolList [] r = r
+forceBoolList (x:xs) r = case x of 
+	True -> forceBoolList xs r
+	False -> forceBoolList xs r
 
---main = print $ length ([True,2<3,undefined]) 
-test = (1,seq undefined [1,2,3])
-
-main = do
-    print $ fst test
 {-
+The forceBoolList fuction does not have the type signature forceBoolList :: [Bool] -> [Bool]
+because there is no point in copying the values to a new list when all you want to do is evaluate the list.
+
 force :: a -> a
 force a = seq a a
 
+seq evaluates it's first argument when the result of seq gets evaluated, so when the first and second argument are the same
+it will evaluate 'a' when the result of seq gets evaluated, which also is 'a' so it's an entirely redundant expression.
 
+http://www.haskell.org/haskellwiki/Seq
 -}
+
+
+main = print $ length (forceBoolList [True,2<3,undefined] "Test") 
