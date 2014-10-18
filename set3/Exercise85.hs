ebf7c793c91227f4e46857d9d553d8a5e199f6e6
Koen Wermer <koenwermer@gmail.com>
2014-10-18 18:42:24 +0200

    8.5

--- set3/Exercise85.hs
+++ set3/Exercise85.hs
@@ -1,19 +1,22 @@
-f x y = (x, [y])
-
-x = f . f . f
+x0 = undefined
+x1 = (x0, x0)
+x2 = (x1, x1)
+x3 = (x2, x2)
+x4 = (x3, x3) -- ...and so on
 
 {-
-x has the following type: 
+x0 has type a, which means you could, for example, apply the first element of (x0, x0) to a boolean value and the second to an int.
+Therefore, x1 must have type (a, b) (both a and b can be choosen freely when applying the functions).
+For the same reason, the type of the first polymorphic tuple x1 in x2, doesn't match the type of the second one (they use different type variables),
+and so on..
 
-a0 -> t0 -> (t1 -> (t2 -> (a0, t2), t1), t0)
+Not that x0 can be any polymorphic function.
 
-which has an amount of type variables equal to 2^N, where N is the number of composed f's.
-Because theres a new type variable required for every level it can not be internally represented using sharing.
+Every line of code doubles the number of type variables in the resulting type, so the type grows exponentially.
 
-Found at:
+Based on:
 
 http://stackoverflow.com/questions/22060592/growth-of-type-definition-in-sml-using-hindley-milner-type-inference
-
 
 -}
 
