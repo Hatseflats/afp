58d7842cda55eddd7a34c0174d62233276b68552
Sebastiaan Jong <sebastiaan.jong@gmail.com>
2014-11-02 17:47:55 +0100

    ..

--- set4/Exercise112.hs
+++ set4/Exercise112.hs
@@ -1,12 +1,31 @@
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Exercise122 where
 
 import GHC.Generics
 
 class GRead a where
-	greadsPrec :: Int -> ReadS a
+    greadsPrec :: Int -> String -> [(a,String)]
 
-instance GRead Char   where greadsPrec = showsPrec
-instance GRead Int    where greadsPrec = showsPrec
-instance GRead Float  where greadsPrec = showsPrec
-instance GRead String where greadsPrec = showsPrec
-instance GRead Bool   where greadsPrec = showsPrec
+instance GRead Char   where greadsPrec = readsPrec
+instance GRead Int    where greadsPrec = readsPrec
+instance GRead Float  where greadsPrec = readsPrec
+instance GRead String where greadsPrec = readsPrec
+instance GRead Bool   where greadsPrec = readsPrec
+
+class GRead' f where
+    greadsPrec' :: Int -> String -> [(f a, String)]
+
+instance GRead' U1 where
+    greadsPrec' _ s = [(U1, s)]
+
+greadsPrecDefault :: (Generic a, GRead' (Rep a)) => Int -> String -> [(a,String)]
+greadsPrecDefault n x = to (fst rep)
+    where (rep:_) = (greadsPrec' n x)
+
+main = do
+    --print $ (greadsPrec' 0 "5" :: [(Int,String)])
+    print $ "t"
